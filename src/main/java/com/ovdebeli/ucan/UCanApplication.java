bff960e1007626ab5b7dac41052a50541f329499
nikolaradovanov <ovdebeli@gmail.com>
2022-10-31 11:30:32 +0100

    Initial project

---
+++ src/main/java/com/ovdebeli/ucan/UCanApplication.java
@@ -0,0 +1,13 @@
+package com.ovdebeli.ucan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UCanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UCanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ovdebeli/ucan/UCanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ovdebeli.ucan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UCanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
