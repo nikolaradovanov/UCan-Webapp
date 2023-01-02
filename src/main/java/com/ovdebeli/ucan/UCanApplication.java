8553fa6012db93ada0a48f7b74899e3c5fafc819
nikolaradovanov <ovdebeli@gmail.com>
2023-01-02 17:06:06 +0100

    Removed properties and bootload

--- src/main/java/com/ovdebeli/ucan/UCanApplication.java
+++ src/main/java/com/ovdebeli/ucan/UCanApplication.java
@@ -30,40 +30,40 @@ public class UCanApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        Author author = new Author("Abraham","Lincoln","Bio");
-        authorService.saveAuthor(author);
-        Category categoryMotivational = new Category("Motivational","Description");
-        categoryService.saveCategory(categoryMotivational);
-        Category categoryMoney = new Category("Money","Description");
-        categoryService.saveCategory(categoryMoney);
-        Category categorySuccess = new Category("Success","Description");
-        categoryService.saveCategory(categorySuccess);
-        Category categoryTruth = new Category("Truth","Description");
-        categoryService.saveCategory(categoryTruth);
-        Quote quote1 = new Quote("Text1",author,categoryMoney);
-        quoteService.saveQuote(quote1);
-        Quote quote2 = new Quote("Text2",author,categoryMoney);
-        quoteService.saveQuote(quote2);
-        Quote quote3 = new Quote("Text3",author,categoryMoney);
-        quoteService.saveQuote(quote3);
-        Quote quote4 = new Quote("Text4",author,categoryMotivational);
-        quoteService.saveQuote(quote4);
-        Quote quote5 = new Quote("Text5",author,categoryMotivational);
-        quoteService.saveQuote(quote5);
-        Quote quote6 = new Quote("Text6",author,categoryMotivational);
-        quoteService.saveQuote(quote6);
-        Quote quote7 = new Quote("Text7",author,categorySuccess);
-        quoteService.saveQuote(quote7);
-        Quote quote8 = new Quote("Text8",author,categorySuccess);
-        quoteService.saveQuote(quote8);
-        Quote quote9 = new Quote("Text9",author,categorySuccess);
-        quoteService.saveQuote(quote9);
-        Quote quote10 = new Quote("Text10",author,categoryTruth);
-        quoteService.saveQuote(quote10);
-        Quote quote11 = new Quote("Text11",author,categoryTruth);
-        quoteService.saveQuote(quote11);
-        Quote quote12 = new Quote("Text12",author,categoryTruth);
-        quoteService.saveQuote(quote12);
+//        Author author = new Author("Abraham","Lincoln","Bio");
+//        authorService.saveAuthor(author);
+//        Category categoryMotivational = new Category("Motivational","Description");
+//        categoryService.saveCategory(categoryMotivational);
+//        Category categoryMoney = new Category("Money","Description");
+//        categoryService.saveCategory(categoryMoney);
+//        Category categorySuccess = new Category("Success","Description");
+//        categoryService.saveCategory(categorySuccess);
+//        Category categoryTruth = new Category("Truth","Description");
+//        categoryService.saveCategory(categoryTruth);
+//        Quote quote1 = new Quote("Text1",author,categoryMoney);
+//        quoteService.saveQuote(quote1);
+//        Quote quote2 = new Quote("Text2",author,categoryMoney);
+//        quoteService.saveQuote(quote2);
+//        Quote quote3 = new Quote("Text3",author,categoryMoney);
+//        quoteService.saveQuote(quote3);
+//        Quote quote4 = new Quote("Text4",author,categoryMotivational);
+//        quoteService.saveQuote(quote4);
+//        Quote quote5 = new Quote("Text5",author,categoryMotivational);
+//        quoteService.saveQuote(quote5);
+//        Quote quote6 = new Quote("Text6",author,categoryMotivational);
+//        quoteService.saveQuote(quote6);
+//        Quote quote7 = new Quote("Text7",author,categorySuccess);
+//        quoteService.saveQuote(quote7);
+//        Quote quote8 = new Quote("Text8",author,categorySuccess);
+//        quoteService.saveQuote(quote8);
+//        Quote quote9 = new Quote("Text9",author,categorySuccess);
+//        quoteService.saveQuote(quote9);
+//        Quote quote10 = new Quote("Text10",author,categoryTruth);
+//        quoteService.saveQuote(quote10);
+//        Quote quote11 = new Quote("Text11",author,categoryTruth);
+//        quoteService.saveQuote(quote11);
+//        Quote quote12 = new Quote("Text12",author,categoryTruth);
+//        quoteService.saveQuote(quote12);
 
     }
 }
